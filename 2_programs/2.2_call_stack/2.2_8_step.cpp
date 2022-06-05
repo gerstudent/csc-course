b1e6ff92c327263995fd806d0fb903535e65ac99
gerstudent <gkolcherin@gmail.com>
2022-06-05 17:56:05 +0300

    Add a description to the tasks

--- 2_programs/2.2_call_stack/2.2_8_step.cpp
+++ 2_programs/2.2_call_stack/2.2_8_step.cpp
@@ -1,6 +1,13 @@
+/*
+    Напишите рекурсивную функцию вычисления наибольшего общего делителя двух положительных
+    целых чисел (Greatest Common Divisor, GCD). Для этого воспользуйтесь следующими свойствами:
+
+    1. GCD(a,b) = GCD(b,a mod b)
+    2. GCD(0,a) = a
+    3. GCD(a,b) = GCD(b,a)
+
+ */
+
 unsigned gcd(unsigned a, unsigned b) {
     return a == 0 ? b : b == 0 ? a : a == b ? a : gcd(b, a % b);
 }
-
-
-
